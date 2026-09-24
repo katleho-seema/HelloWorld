a420563b46d4d4ef9c45a6b6434a8a89f775b8fe
Katleho Seema <katlehoseema7@gmail.com>
2026-09-24 11:55:11 +0200

    My First Program

---
+++ Practice Codes/HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int SIZE = 6;
+
+    int numbers[SIZE] = {1,24,13,0,3};
+
+    for (int i = 0; i< SIZE ; i++) {
+        cout << numbers[i] << " ";
+
+    }
+    
+
+
+
+
+    return 0;
+}
