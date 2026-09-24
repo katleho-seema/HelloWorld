82a66686b2788b4ed758c254f5a3c0abb302ae89
Katleho Seema <katlehoseema7@gmail.com>
2026-09-24 18:16:44 +0200

    Operations Added

--- Practice Codes/HelloWorld.cpp
+++ Practice Codes/HelloWorld.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    string name = "Katleho";
+    /*string name = "Katleho";
     int age = 12;
     double height = 1.55;
     char letter = 'N';
@@ -12,6 +12,20 @@ int main(){
     cout << "My name is " << name << endl;
     cout << "I am " << age << " years old." << endl;
     cout << "My height is " << height << endl;
-    cout << "Do I like coding? " << coding << endl;
+    cout << "Do I like coding? " << coding << endl;*/
+    // Operations
+
+    int num1;
+    int num2;
+
+    cout << "Enter your first number: ";
+    cin >> num1;
+    cout << "Enter your second number: ";
+    cin >> num2;
+
+    int Total = num1 + num2;
+    
+    cout << Total << endl;
+    
     return 0;
 }
